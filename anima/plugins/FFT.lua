53aad2669244765f99cd17f0b78207829822d60b
sonoro1234 <sonoro@telefonica.net>
2020-01-10 10:58:42 +0100

    FFT improvements

--- anima/plugins/FFT.lua
+++ anima/plugins/FFT.lua
@@ -1,3 +1,8 @@
+--From
+--#Fourier Image Filtering
+--[http://david.li/filtering](http://david.li/filtering)
+--
+
 local 	PING_TEXTURE_UNIT = 0
 local    PONG_TEXTURE_UNIT = 1
 local    FILTER_TEXTURE_UNIT = 2
@@ -14,8 +19,8 @@ local INVERSE = 1;
 
 
 local FULLSCREEN_VERTEX_SOURCE = [[
-    attribute vec2 a_position;
-    varying vec2 v_coordinates; //this might be phased out soon (no pun intended)
+    in vec2 a_position;
+    out vec2 v_coordinates; //this might be phased out soon (no pun intended)
     void main (void) {
         v_coordinates = a_position * 0.5 + 0.5;
         gl_Position = vec4(a_position, 0.0, 1.0);
@@ -85,7 +90,7 @@ local SUBTRANSFORM_FRAGMENT_SOURCE = [[
 ]]
 
 local FILTER_FRAGMENT_SOURCE = [[
-    precision highp float;
+    //precision highp float;
 
     uniform sampler2D u_input;
     uniform float u_resolution;
@@ -110,9 +115,9 @@ local FILTER_FRAGMENT_SOURCE = [[
 ]]
 
 local POWER_FRAGMENT_SOURCE = [[
-    precision highp float;
+    //precision highp float;
 
-    varying vec2 v_coordinates;
+    in vec2 v_coordinates;
 
     uniform sampler2D u_spectrum;
     uniform float u_resolution;
@@ -148,19 +153,20 @@ local POWER_FRAGMENT_SOURCE = [[
         float bPower = length(b);
 
         float averagePower = (rPower + gPower + bPower) / 3.0;
-        gl_FragColor = encodeFloat(averagePower / (u_resolution * u_resolution));
+        //gl_FragColor = encodeFloat(averagePower / (u_resolution * u_resolution));
+		gl_FragColor = vec4(averagePower / (u_resolution * u_resolution));
     }
 ]]
 
 local IMAGE_FRAGMENT_SOURCE = [[
-    precision highp float;
+    //precision highp float;
 
-    varying vec2 v_coordinates;
+    in vec2 v_coordinates;
 
-    uniform float u_resolution;
+    //uniform float u_resolution;
 
     uniform sampler2D u_texture;
-    uniform sampler2D u_spectrum;
+    //uniform sampler2D u_spectrum;
 
     void main (void) {
         vec3 image = texture2D(u_texture, v_coordinates).rgb;
@@ -193,7 +199,9 @@ local function buildTexture( unit, format, type, width, height, data, wrapS, wra
     return texture;
 end
 
+local texturebyunit = {}
 local function bindtex(unit,tex, wrapS, wrapT, minFilter, magFilter)
+	texturebyunit[unit] = tex
 	glext.glActiveTexture(glc.GL_TEXTURE0 + unit);
 	gl.glBindTexture(glc.GL_TEXTURE_2D, tex);
 	gl.glTexParameteri(glc.GL_TEXTURE_2D, glc.GL_TEXTURE_WRAP_S, wrapS);
@@ -215,21 +223,38 @@ local function Filterer(GL,args)
 	local RESOLUTION = args.RES;
 	FF.RESOLUTION = RESOLUTION
 	local END_EDIT_FREQUENCY = 150.0;
-	local END_EDIT_FREQUENCY2 = math.sqrt(((RESOLUTION*0.5)^2)*2)
+	local END_EDIT_FREQUENCY2 = RESOLUTION*0.5 --math.sqrt(((RESOLUTION*0.5)^2)*2)
 	print("END_EDIT_FREQUENCY2",END_EDIT_FREQUENCY2)
-	
+	END_EDIT_FREQUENCY = END_EDIT_FREQUENCY2
 	--------------
 	local plugin = require"anima.plugins.plugin"
 	local presets --= plugin.presets(FF)
 	local serializer --= plugin.serializer(FF)
 	
+	local function setcurve(NM)
+		local wi = NM.width
+		local gain = NM.gain*0.5
+		local peak = NM.peak/END_EDIT_FREQUENCY
+		local min = math.max(0,peak-wi)
+		local max = math.min(1,peak+wi)
+		NM.defs.curv.curve:setpoints{{x=min,y=0.5},{x=peak,y=gain},{x=max,y=0.5},{x=-1,y=0}} 
+		--NM.defs.curv.curve:setpoints{{x=min,y=0.5},{x=min,y=gain},{x=peak,y=gain},{x=max,y=gain},{x=max,y=0.5},{x=-1,y=0}}
+	end
+	
+	local powersdata = ffi.new("float[?]",END_EDIT_FREQUENCY)
+	local maxvalp = ffi.new("float[1]",0.01)
 	local NM = GL:Dialog("fft",
-	{{"unit",6,guitypes.valint,{min=0,max=6}},
+	{{"unit",6,guitypes.valint,{min=0,max=7}},
 	{"curv",{0,0.5,1,0.5},guitypes.curve,{pressed_on_modified=false},function(curve) FF:filter(curve.LUT,curve.LUTsize) end},
+	{"peak",0.5,guitypes.val,{min=0,max=END_EDIT_FREQUENCY},function(val,this) setcurve(this) end},
+	{"width",0.1,guitypes.val,{min=0,max=0.1},function(val,this) setcurve(this) end},
+	{"gain",1,guitypes.val,{min=0,max=2},function(val,this) setcurve(this) end},
 	{"bypass",false,guitypes.toggle},
 	},function(this) 
 		presets.draw()
 		serializer.draw()
+		ig.SliderFloat("max", maxvalp, 0, 0.01, "%0.4f", 1);
+		ig.PlotLines("powers", powersdata, END_EDIT_FREQUENCY, 0, nil, 0,maxvalp[0], ig.ImVec2(400,200));
 	end)
 
 
@@ -249,6 +274,7 @@ local function Filterer(GL,args)
 	local old_framebuffer = ffi.new("GLuint[1]",0)
 	function FF:saveoldFBO()
 		gl.glGetIntegerv(glc.GL_DRAW_FRAMEBUFFER_BINDING, old_framebuffer)
+		print("saveoldFBO",old_framebuffer[0])
 	end
 	function FF:setoldFBO()
 	print("setoldFBO",old_framebuffer[0])
@@ -261,7 +287,8 @@ local function Filterer(GL,args)
 		bindtex(FILTER_TEXTURE_UNIT,filterTexture,glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
 		bindtex(ORIGINAL_SPECTRUM_TEXTURE_UNIT,originalSpectrumTexture, glc.GL_REPEAT, glc.GL_REPEAT, glc.GL_NEAREST, glc.GL_NEAREST)
 		bindtex(FILTERED_SPECTRUM_TEXTURE_UNIT,filteredSpectrumTexture, glc.GL_REPEAT, glc.GL_REPEAT, glc.GL_NEAREST, glc.GL_NEAREST)
-		bindtex(FILTERED_IMAGE_TEXTURE_UNIT,filteredImageTexture, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
+		--bindtex(FILTERED_IMAGE_TEXTURE_UNIT,filteredImageTexture, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
+		bindtex(FILTERED_IMAGE_TEXTURE_UNIT,filteredImageTexture, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_LINEAR_MIPMAP_LINEAR, glc.GL_LINEAR)
 		bindtex(READOUT_TEXTURE_UNIT,readoutTexture, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
 	end
 	function FF:init()
@@ -273,7 +300,7 @@ local function Filterer(GL,args)
         originalSpectrumTexture = buildTexture( ORIGINAL_SPECTRUM_TEXTURE_UNIT, glc.GL_RGBA, glc.GL_FLOAT, RESOLUTION, RESOLUTION, nil, glc.GL_REPEAT, glc.GL_REPEAT, glc.GL_NEAREST, glc.GL_NEAREST)
         filteredSpectrumTexture = buildTexture( FILTERED_SPECTRUM_TEXTURE_UNIT, glc.GL_RGBA, glc.GL_FLOAT, RESOLUTION, RESOLUTION, nil, glc.GL_REPEAT, glc.GL_REPEAT, glc.GL_NEAREST, glc.GL_NEAREST)
         filteredImageTexture = buildTexture( FILTERED_IMAGE_TEXTURE_UNIT, glc.GL_RGBA, glc.GL_FLOAT, RESOLUTION, RESOLUTION, nil, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
-        readoutTexture = buildTexture( READOUT_TEXTURE_UNIT, glc.GL_RGBA, glc.GL_UNSIGNED_BYTE, RESOLUTION, RESOLUTION, nil, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
+        readoutTexture = buildTexture( READOUT_TEXTURE_UNIT, glc.GL_RGBA, glc.GL_FLOAT, RESOLUTION, RESOLUTION, nil, glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
 
 		pingFramebuffer = buildFramebuffer( pingTexture)
         pongFramebuffer = buildFramebuffer( pongTexture)
@@ -336,7 +363,6 @@ local function Filterer(GL,args)
 
             if (direction == INVERSE and i == 0) then
                 subtransformProgramWrapper.unif.u_normalize:set{true}
-            --elseif (direction == INVERSE and i == 1) then
 			else
                 subtransformProgramWrapper.unif.u_normalize:set{false}
             end
@@ -372,6 +398,7 @@ local function Filterer(GL,args)
 
         self:fft(IMAGE_TEXTURE_UNIT, originalSpectrumFramebuffer, RESOLUTION, RESOLUTION, FORWARD);
     end
+
 	local oldtexsignature
 	function FF:set_texture(tex)
 		if oldtexsignature and oldtexsignature==tex:get_signature() then return end
@@ -381,14 +408,62 @@ local function Filterer(GL,args)
 		if imageTexture then imageTexture:delete() end
 		imageTexture = tex:resample(RESOLUTION,RESOLUTION)
 		imageTexture:Bind(IMAGE_TEXTURE_UNIT)
-		imageTexture:set_wrap(glc.GL_CLAMP_TO_EDGE)
-		imageTexture:mag_filter(glc.GL_NEAREST)
-		imageTexture:min_filter(glc.GL_NEAREST)
-		
+		imageTexture:gen_mipmap()
+		-- imageTexture:Bind(IMAGE_TEXTURE_UNIT)
+		-- imageTexture:set_wrap(glc.GL_CLAMP_TO_EDGE)
+		-- imageTexture:mag_filter(glc.GL_NEAREST)
+		-- imageTexture:min_filter(glc.GL_NEAREST)
+		bindtex(IMAGE_TEXTURE_UNIT,imageTexture.tex,glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
 		self:bindtexs()
 		glext.glActiveTexture(glc.GL_TEXTURE0 + ORIGINAL_SPECTRUM_TEXTURE_UNIT);
         gl.glTexImage2D(glc.GL_TEXTURE_2D, 0, glc.GL_RGBA32F, RESOLUTION, RESOLUTION, 0, glc.GL_RGBA, glc.GL_FLOAT, nil);
 		self:fft(IMAGE_TEXTURE_UNIT, originalSpectrumFramebuffer, RESOLUTION, RESOLUTION, FORWARD);
+		
+		-------readout power
+		self:bindtexs()
+		glext.glBindFramebuffer(glc.GL_FRAMEBUFFER, readoutFramebuffer);
+        gl.glViewport(0, 0, RESOLUTION, RESOLUTION);
+		readoutProgram:use()
+		readoutProgramvao:draw(glc.GL_TRIANGLE_STRIP)
+		
+		local numpixels = RESOLUTION*RESOLUTION*1
+		local pixelsUserData = ffi.new("float[?]",numpixels)
+		glext.glBindFramebuffer(glc.GL_READ_FRAMEBUFFER, readoutFramebuffer);
+		gl.glReadBuffer(glc.GL_COLOR_ATTACHMENT0 + 0); 
+		
+		glext.glBindBuffer(glc.GL_PIXEL_PACK_BUFFER,0)
+		gl.glPixelStorei(glc.GL_PACK_ALIGNMENT, 1)
+		gl.glReadPixels(0,0, RESOLUTION, RESOLUTION, glc.GL_RED, glc.GL_FLOAT, pixelsUserData)
+		
+		local maxval = -math.huge
+		for i=0,numpixels-1 do
+			if pixelsUserData[i]>maxval then maxval = pixelsUserData[i] end
+		end
+		
+		print("maxval",maxval)
+		local powersByFrequency = {}
+		local pixelIndex = 0
+		for yIndex=0,RESOLUTION-1 do
+			local y = yIndex - RESOLUTION / 2;
+			for xIndex=0,RESOLUTION-1 do
+				local x = xIndex - RESOLUTION / 2
+				local frequency = math.floor(0.5+math.sqrt(x * x + y * y))
+                powersByFrequency[frequency] = powersByFrequency[frequency] or {}
+				table.insert(powersByFrequency[frequency],pixelsUserData[pixelIndex])
+				pixelIndex = pixelIndex + 1
+			end
+		end
+		
+		local data = {}
+		for f,arr in pairs(powersByFrequency) do
+			for i,v in ipairs(arr) do
+				data[f] = v + (data[f] or 0)
+			end
+			data[f] = data[f]/#arr
+		end
+		for i=0,END_EDIT_FREQUENCY-1 do
+			powersdata[i] = data[i] or 0
+		end
 		
 		self:setoldFBO()
 	end
@@ -418,29 +493,33 @@ local function Filterer(GL,args)
 		self:output2(NM.unit)
     end
 	function FF:output2(nn) 
+		print"fft output"
 		--ut.Clear()
 		if nn==6 or nn==5 then
 			local xoff,yoff= 0,0
 			local w,h = GL.W,GL.H
 			if GL.H > GL.W then
-				xoff = (GL.W-GL.H)*0.5
+				xoff = math.floor((GL.W-GL.H)*0.5+0.5)
 				w = GL.H
 			else
-				yoff = (GL.H-GL.W)*0.5
+				yoff = math.floor((GL.H-GL.W)*0.5+0.5)
 				h = GL.W
 			end
 			gl.glViewport(xoff,yoff,w,h)
+			bindtex(nn,texturebyunit[nn],glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_LINEAR_MIPMAP_LINEAR, glc.GL_LINEAR)
 		else
 			gl.glViewport(getAspectViewport(GL.W,GL.H,RESOLUTION, RESOLUTION));
 		end
-        imageProgram:use()
-		imageProgram.unif.u_resolution:set{RESOLUTION}
+		
+		imageProgram:use()
 		imageProgram.unif.u_texture:set{nn}
-		imageProgram.unif.u_spectrum:set{FILTERED_SPECTRUM_TEXTURE_UNIT}
 		imageProgramvao:draw(glc.GL_TRIANGLE_STRIP)
+		
+		bindtex(IMAGE_TEXTURE_UNIT,imageTexture.tex,glc.GL_CLAMP_TO_EDGE, glc.GL_CLAMP_TO_EDGE, glc.GL_NEAREST, glc.GL_NEAREST)
+		self:bindtexs()
     end
 	function FF:process(texture)
-		print"fftprocess"
+		print"---------------fftprocess"
 		if NM.bypass then texture:drawcenter();return end
 		self:set_texture(texture)
 		self:filter(curve.LUT,curve.LUTsize)
@@ -486,35 +565,73 @@ end
 GL:start()
 --]=]
 
----[=[
+--[==[
 require"anima"
-local GL = GLcanvas{H=700,W=700}
+local RES = 512*2
+local GL = GLcanvas{H=RES,W=RES,vsync=true,SDL=false}
 
 local NM = GL:Dialog("test",{
-{"freq",1,guitypes.val,{min=1,max=500}}
+{"freq",100,guitypes.val,{min=1,max=RES/2}},
+{"turns",0,guitypes.val,{min=0,max=1}}
 })
-local tproc,chain,fft
+local tproc,chain,fft,fbo,fbo2
 function GL.init()
 	tproc = require"anima.plugins.texture_processor"(GL,0,NM)
-	tproc:set_process[[vec4 process(vec2 pos){
-		return vec4(sin(pos.x*2*3.14159*freq)*0.5+0.5);
+	---[=[
+	tproc:set_process[[
+	#define M_PI 3.1415926535897932384626433832795
+	vec4 process(vec2 pos){
+		float angle = M_PI*turns*2;
+		vec2 dir = vec2(cos(angle),sin(angle));
+		float dis = dot(dir,pos);
+		return vec4(sin(dis*2*M_PI*freq)*0.5+0.5);
 	}]]
+	--]=]
+	--[=[
+	tproc:set_process[[vec4 process(vec2 pos){
+		return vec4(sin(pos.y*2*3.14159*freq)*0.5+0.5);
+	}]]
+	--]=]
+	--[=[
+	tproc:set_process[[
+		float randhash(uint seed, float b)
+{
+    const float InverseMaxInt = 1.0 / 4294967295.0;
+    uint i=(seed^12345391u)*2654435769u;
+    i^=(i<<6u)^(i>>26u);
+    i*=2654435769u;
+    i+=(i<<5u)^(i>>12u);
+    return float(b * i) * InverseMaxInt;
+}
+	vec4 process(vec2 pos){
+		vec2 fcoord = gl_FragCoord.xy;
+		uint seed = uint(fcoord.x) * uint(fcoord.y);
+		return vec4(randhash(seed,1));
+	}
+	]]
+	--]=]
 	fft = Filterer(GL)
 	local tex = GL:Texture()
 	local tex2 = tex:resample(2,2)
+	fbo = GL:initFBO{no_depth=true}
+	fbo2 = GL:initFBO{no_depth=true}
 	chain = tex:make_chain{tproc,fft}
 	GL:DirtyWrap()
 end
 
 function GL.draw(t,w,h)
 	ut.Clear()
-	--tproc:process{}
+	-- tproc:process_fbo(fbo,{})
+	-- fft:process_fbo(fbo2,fbo:tex())
+	-- fbo2:tex():drawcenter()
 	chain:process({})
 	chain:tex():drawcenter()
+	--io.read"*l"
+	--print"frame"
 end
 
 GL:start()
---]=]
+--]==]
 
 
 return Filterer
