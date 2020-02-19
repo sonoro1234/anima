b82cb40d173ed4764a32754a2c24be3dbac5fd44
Victor Bombi <sonoroster@gmail.com>
2020-02-19 16:38:56 +0100

    plugins : morphology : refactor and add cross variable

--- anima/plugins/morphology.lua
+++ anima/plugins/morphology.lua
@@ -31,21 +31,22 @@ local frag_erode = [[
 
 uniform sampler2D tex;
 uniform int kernelsize = 1;
+uniform bool cross;
 vec2 texs = textureSize(tex,0);
 void main()
 {
 	
 	float valmin = 10;
+	int rest;
 	for(int x=-kernelsize;x<=kernelsize;x++){
-		for(int y=-kernelsize;y<=kernelsize;y++){
+		if(cross)
+			rest = kernelsize - abs(x);
+		else
+			rest = kernelsize;
+		for(int y=-rest;y<=rest;y++){
 			float val = texelFetch(tex,ivec2(gl_FragCoord.xy)+ivec2(x,y),0).r;
 			//float val = texture(tex,(gl_FragCoord.xy+vec2(x,y))/texs,0).r;
 			valmin = min(valmin,val);
-			//if(valmin==0.0){
-			//	gl_FragColor = vec4(vec3(0.0),1);
-			//	return;
-			//}
-			
 		}
 	}
 	gl_FragColor = vec4(vec3(valmin),1);
@@ -75,9 +76,10 @@ local function morphology(GL,args)
 	local plugin = require"anima.plugins.plugin"
 	local M = plugin.new({res={GL.W,GL.H}},GL)
 	local NM = GL:Dialog("morphol",
-	{{"op",0,guitypes.combo,{"erode","dilate","open","close","TopHatW","TopHatB"}},
+	{{"op",0,guitypes.combo,{"erode","dilate","open","close","TopHatW","TopHatB","border"}},
 	{"kernelsize",1,guitypes.valint,{min=0,max=10}},
 	{"iters",1,guitypes.valint,{min=1,max=30}},
+	{"cross",false,guitypes.toggle},
 	{"doneg",false,guitypes.toggle},
 	{"bypass",false,guitypes.toggle}
 	})
@@ -133,36 +135,29 @@ local function morphology(GL,args)
 			program2 = program_erode
 			quad2 = quade
 			subs = true
+		elseif NM.op == 6 then
+			program = program_erode
+			quad = quade
+			program2 = nil
+			subs = true
 		end
-		if not program2 then
+
+		slab:Bind()
+		program:use()
+		program.unif.kernelsize:set{NM.kernelsize}
+		program.unif.cross:set{NM.cross}
+		program.unif.tex:set{0}
+		gl.glViewport(0,0,w or self.res[1], h or self.res[2])
+		quad:draw_elm()
+		slab:UnBind()
+		for i=2,NM.iters do
+			slab:tex():Bind()
 			slab:Bind()
-			program:use()
-			program.unif.kernelsize:set{NM.kernelsize}
-			program.unif.tex:set{0}
-			gl.glViewport(0,0,w or self.res[1], h or self.res[2])
 			quad:draw_elm()
 			slab:UnBind()
-			for i=2,NM.iters do
-				slab:tex():Bind()
-				slab:Bind()
-				quad:draw_elm()
-				slab:UnBind()
-			end
-			slab:tex():drawcenter()
-		else
-			slab:Bind()
-			program:use()
-			program.unif.kernelsize:set{NM.kernelsize}
-			program.unif.tex:set{0}
-			gl.glViewport(0,0,w or self.res[1], h or self.res[2])
-			quad:draw_elm()
-			slab:UnBind()
-			for i=2,NM.iters do
-				slab:tex():Bind()
-				slab:Bind()
-				quad:draw_elm()
-				slab:UnBind()
-			end
+		end
+
+		if program2 then
 			slab:tex():Bind()
 			slab:Bind()
 			program2:use()
@@ -177,24 +172,24 @@ local function morphology(GL,args)
 				quad2:draw_elm()
 				slab:UnBind()
 			end
-			if not subs then 
-				slab:tex():drawcenter()
-			else
-				if NM.op == 4 then
-					srctex:Bind(0)
-					slab:tex():Bind(1)
-				else
-					srctex:Bind(1)
-					slab:tex():Bind(0)
-				end
-				program_substract:use()
-				program_substract.unif.tex:set{0}
-				program_substract.unif.tex2:set{1}
-				program_substract.unif.doneg:set{NM.doneg}
-				gl.glViewport(0,0,w or self.res[1], h or self.res[2])
-				--ut.Clear()
-				quads:draw_elm()
+		end
+		if not subs then 
+			slab:tex():drawcenter()
+		else
+			if NM.op == 4 or NM.op == 6 then
+				srctex:Bind(0)
+				slab:tex():Bind(1)
+			else -- op==5
+				srctex:Bind(1)
+				slab:tex():Bind(0)
 			end
+			program_substract:use()
+			program_substract.unif.tex:set{0}
+			program_substract.unif.tex2:set{1}
+			program_substract.unif.doneg:set{NM.doneg}
+			gl.glViewport(0,0,w or self.res[1], h or self.res[2])
+			--ut.Clear()
+			quads:draw_elm()
 		end
 	end
 	GL:add_plugin(M,"morphology")
@@ -202,14 +197,16 @@ local function morphology(GL,args)
 end
 
 --[=[
-local GL = GLcanvas{H=700,aspect=1}
+local GL = GLcanvas{H=700,aspect=1,fbo_nearest=true}
 
 NM = GL:Dialog("test",{
 	{"orig",false,guitypes.toggle},
+	{"rep",false,guitypes.toggle},
 })
 
-fileName = [[C:\luaGL\frames_anima\msquares\imagen2.tif]]
---fileName = [[C:\luaGL\frames_anima\im_test\Cosmos_original.jpg]]
+--fileName = [[C:\luaGL\frames_anima\msquares\imagen2.tif]]
+fileName = [[C:\luaGL\frames_anima\im_test\Cosmos_original.jpg]]
+fileName = [[C:\luaGL\frames_anima\flood_fill\dummy.png]]
 --fileName = [[C:\luaGL\frames_anima\im_test\unnamed0.jpg]]
 local im = require"imffi"
 local vicim = require"anima.vicimag"
@@ -224,9 +221,9 @@ function GL.init()
 	texture = pd:totex(GL) --GL:Texture():Load(fileName)
 	GL:set_WH(texture.width,texture.height)
 	fx = morphology(GL,{fixed_fbos=true})
-	fx.NM.vars.op[0]=4
+	fx.NM.vars.op[0]=6
 	fbo = GL:initFBO({no_depth=true})
-	--fbo2 = GL:initFBO({no_depth=true})
+	fbo2 = GL:initFBO({no_depth=true})
 	GL:DirtyWrap()
 end
 
@@ -236,9 +233,15 @@ function GL.draw(t,w,h)
 	if NM.orig then
 		texture:drawcenter(w,h)
 	else
-
+		--fx.NM:SetValues{op="border"}
 		fx:process_fbo(fbo,texture)
+		if NM.rep then
+		fx.NM:SetValues{op="erode"}
+		fx:process_fbo(fbo2,fbo:tex())
+		fbo2:tex():drawcenter()
+		else
 		fbo:tex():drawcenter()
+		end
 	end
 end
 
